b15ff55550bc59fba5cca0a7f0732d49344d80d1
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-02-07 21:10:14 +0530

    Added root A record

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -1,3 +1,12 @@
+resource "cloudflare_dns_record" "root" {
+  zone_id = var.cloudflare_zone_id
+  content = "212.2.243.234"
+  name    = "isujith.dev"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+}
+
 resource "cloudflare_dns_record" "cv" {
   zone_id = var.cloudflare_zone_id
   content = "212.2.243.234"
