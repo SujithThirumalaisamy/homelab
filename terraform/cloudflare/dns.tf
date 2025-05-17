9a9c3c261d983809004e0e42b03454773a930a62
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-05-17 20:35:26 +0530

    New dns state

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -78,3 +78,53 @@ resource "cloudflare_dns_record" "omara" {
   ttl     = 1
   type    = "A"
 }
+
+resource "cloudflare_dns_record" "ngrok" {
+  zone_id = var.cloudflare_zone_id
+  content = "212.2.243.86"
+  name    = "*.isujith.dev"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+}
+
+resource "cloudflare_dns_record" "mandrill" {
+  zone_id = var.cloudflare_zone_id
+  content = "mandrill_verify.yi94aWK89PU7d7hVDxkPSQ"
+  name    = "isujith.dev"
+  ttl     = 1
+  type    = "TXT"
+}
+
+resource "cloudflare_dns_record" "mandrill_mte1" {
+  zone_id = var.cloudflare_zone_id
+  content = "dkim1.mandrillapp.com"
+  name    = "mte1._domainkey.isujith.dev"
+  ttl     = 1
+  type    = "CNAME"
+}
+
+resource "cloudflare_dns_record" "mandrill_mte2" {
+  zone_id = var.cloudflare_zone_id
+  content = "dkim2.mandrillapp.com"
+  name    = "mte2._domainkey.isujith.dev"
+  ttl     = 1
+  type    = "CNAME"
+}
+
+resource "cloudflare_dns_record" "mandrill_dmarc" {
+  zone_id = var.cloudflare_zone_id
+  content = "v=DMARC1; p=none;"
+  name    = "_dmarc.isujith.dev"
+  ttl     = 1
+  type    = "TXT"
+}
+
+resource "cloudflare_dns_record" "yash" {
+  zone_id = var.cloudflare_zone_id
+  content = "165.227.240.66"
+  name    = "yash.isujith.dev"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+}
