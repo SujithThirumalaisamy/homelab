a14a3b87f3f042dcd2d39c14af2d2e774367c213
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-05-27 01:48:12 +0530

    Migrating to Civo India

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_dns_record" "root" {
   zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
+  content = "212.2.250.163"
   name    = "isujith.dev"
   proxied = true
   ttl     = 1
@@ -9,71 +9,8 @@ resource "cloudflare_dns_record" "root" {
 
 resource "cloudflare_dns_record" "cv" {
   zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
+  content = "212.2.250.163"
   name    = "cv.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "street-view" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "street-view.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "argo" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "argo.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "argo-grpc" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "grpc.argo.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "contest" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "contest.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "contest-api" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "api.contest.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "omara-api" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "api-omara.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-}
-
-resource "cloudflare_dns_record" "omara" {
-  zone_id = var.cloudflare_zone_id
-  content = "212.2.243.234"
-  name    = "omara.isujith.dev"
   proxied = true
   ttl     = 1
   type    = "A"
@@ -81,7 +18,7 @@ resource "cloudflare_dns_record" "omara" {
 
 resource "cloudflare_dns_record" "ngrok" {
   zone_id = var.cloudflare_zone_id
-  content = "212.2.243.86"
+  content = "212.2.250.163"
   name    = "*.isujith.dev"
   proxied = true
   ttl     = 1
@@ -118,13 +55,4 @@ resource "cloudflare_dns_record" "mandrill_dmarc" {
   name    = "_dmarc.isujith.dev"
   ttl     = 1
   type    = "TXT"
-}
-
-resource "cloudflare_dns_record" "yash" {
-  zone_id = var.cloudflare_zone_id
-  content = "165.227.240.66"
-  name    = "yash.isujith.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
 }
