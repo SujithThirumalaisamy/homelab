1812832ccc66c9af72ca47ed837374de5c34c03e
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2026-01-21 21:56:23 +0530

    Added some more services and DNS

--- terraform/dns/lxc.tf
+++ terraform/dns/lxc.tf
@@ -12,3 +12,17 @@ resource "dns_a_record_set" "homeassistant" {
   addresses = ["10.0.5.1"]
   ttl       = 3600
 }
+
+resource "dns_a_record_set" "mainsail" {
+  zone      = "home.isujith.dev."
+  name      = "mainsail"
+  addresses = ["10.0.10.12"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "tplink" {
+  zone      = "home.isujith.dev."
+  name      = "tplink"
+  addresses = ["10.0.100.100"]
+  ttl       = 3600
+}
