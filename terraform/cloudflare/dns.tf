02b0521a99a69d66b710d1052be3d819cc90ddd5
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2026-01-27 03:28:12 +0530

    Added prometheus and grafana hosts

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -34,6 +34,24 @@ resource "cloudflare_dns_record" "argo" {
   type    = "A"
 }
 
+resource "cloudflare_dns_record" "prometheus" {
+  zone_id = var.cloudflare_zone_id
+  content = "212.2.254.75"
+  name    = "prometheus.isujith.dev"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+}
+
+resource "cloudflare_dns_record" "prometheus" {
+  zone_id = var.cloudflare_zone_id
+  content = "212.2.254.75"
+  name    = "grafana.isujith.dev"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+}
+
 resource "cloudflare_dns_record" "mandrill" {
   zone_id = var.cloudflare_zone_id
   content = "mandrill_verify.yi94aWK89PU7d7hVDxkPSQ"
@@ -78,7 +96,7 @@ resource "cloudflare_dns_record" "plex" {
   zone_id = var.cloudflare_zone_id
   content = "10.0.60.10"
   name    = "plex.home.isujith.dev"
-  proxied = false
-  ttl     = 3600
+  proxied = true
+  ttl     = 1
   type    = "A"
 }
